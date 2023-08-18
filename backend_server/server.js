369ebc7d10205bd793a52c447883803a1003d34a
Musarraf <muhammedmusarraf@gmail.com>
2023-08-18 18:31:52 +0530

    Added MERN Stack movie review application with CRUD operation

---
+++ backend_server/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors"); // To Avoid Network issues
+
+// Databases
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+  const movieSchema = new mongoose.Schema({
+    name: String,
+    review: String,
+  });
+
+  // Create Model
+  const Movie = mongoose.model("Movie", movieSchema);
+  // const Avatar = new Movie({ name: "Avatar", review: "Amazing" });
+  // await Avatar.save()
+
+  const app = express();
+  app.use(cors());
+  app.use(express.json()); // !Add this so that when getting data it will get as a json Data when reciving it.
+  app.use(express.urlencoded({ extended: false }));
+
+  app.get("/get", async (req, res) => {
+    const all_movies = await Movie.find({});
+    res.send(all_movies);
+  });
+
+  app.post("/api/insert", async (req, res) => {
+    const movieName = req.body.name; //! Getting from api parameter.
+    const movieReview = req.body.review;
+    const newMovie = new Movie({ name: movieName, review: movieReview });
+    await newMovie
+      .save()
+      .then((data) => {
+        console.log("Movie Review Added", data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  app.delete("/delete/:movieName", async (req, res) => {
+    const recievedName = req.params.movieName; //! Getting from url
+    // Delete a single document
+    await Movie.deleteOne({ name: recievedName }); // returns {deletedCount: 1}
+  });
+
+  app.put("/update", async (req, res) => {
+    const selectedName = req.body.name;
+    const newReview = req.body.review;
+    // Update a single document
+    await Movie.updateOne({ name: selectedName }, { review: newReview });
+  });
+
+  app.listen(3001, function () {
+    // in port 3000 our frontend is hosted.
+    console.log("Server has started.");
+  });
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Axios from "axios"; // This is for requseting api/POST requset
+import "./App.css";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [movieReview, setMovieReview] = useState("");
+  const [movieReviewList, setMovieReviewList] = useState([]);
+
+  const [newReview, setNewReview] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/get").then((response) => {
+      setMovieReviewList(response.data);
+    });
+  }, [movieReviewList]);
+
+  function submitionClicked() {
+    Axios.post("http://localhost:3001/api/insert", {
+      name: movieName,
+      review: movieReview,
+    }).then(() => {
+      alert("Successfull Inster");
+    });
+    setMovieName("");
+    setMovieReview("");
+  }
+
+  function deleteReview(movie) {
+    Axios.delete("http://localhost:3001/delete/" + movie);
+  }
+
+  function updateReview(movie) {
+    Axios.put("http://localhost:3001/update", {
+      name: movie,
+      review: newReview,
+    });
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Review CRUD Application</h1>
+      <div className="form">
+        <label>Movie Name</label>
+        <input
+          type="text"
+          name="movieName"
+          value={movieName}
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        />
+        <label>Movie Review</label>
+        <input
+          type="text"
+          name="review"
+          value={movieReview}
+          onChange={(e) => {
+            setMovieReview(e.target.value);
+          }}
+        />
+        <Button variant="contained" onClick={submitionClicked} >
+          Submit
+        </Button>
+
+        {movieReviewList.map((eachMovie) => {
+          return (
+            <div className="card">
+              <h1>{eachMovie.name}</h1>
+              <p>{eachMovie.review}</p>
+
+              <Button
+                variant="contained"
+                startIcon={<DeleteIcon />}
+                onClick={() => {
+                  deleteReview(eachMovie.name);
+                }}
+                color="error"
+              >
+                Delete
+              </Button>
+
+              <input
+                type="text"
+                id="updateInput"
+                onChange={(e) => setNewReview(e.target.value)}
+              ></input>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  updateReview(eachMovie.name);
+                }}
+              >
+                Update
+              </Button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
